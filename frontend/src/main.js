48f6b567fb29dac7339cb51aae26dcfb6f9a7b9c
Armagan Amcalar <armagan@amcalar.com>
2020-03-10 23:00:59 +0300

    Add ant-design-vue dependency

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'ant-design-vue/dist/antd.css'
 
 Vue.config.productionTip = false
 
