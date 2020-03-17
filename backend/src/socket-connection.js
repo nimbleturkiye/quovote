3d312313d54efb1c3ee827b4763b8ef30aa16c4a
Armagan Amcalar <armagan@amcalar.com>
2020-03-17 21:50:33 +0100

    Implement socket.io connection on the backend

---
+++ backend/src/socket-connection.js
@@ -0,0 +1,16 @@
+const io = require('socket.io')
+let socketServer = null
+
+module.exports = (app, server) => {
+  if (socketServer) return socketServer
+
+  socketServer = io(server)
+
+  socketServer.on('connection', socket => {
+    socket.on('join-room', eventId => {
+      socket.join(eventId)
+    })
+  })
+
+  return socketServer
+}
