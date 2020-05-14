137a0616ea873a933acf0e99ec246c6dd6df1255
Armagan Amcalar <armagan@amcalar.com>
2020-05-14 02:37:51 +0200

    Replace ant-design-vue layout with a custom layout that actually works

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -86,58 +86,103 @@ export default {
 </script>
 
 <template lang="pug">
-a-layout.home
-  a-layout-content
-    a-row(type="flex" justify="center")
-      a-col(span="12" :sm="16" :xs="24")
-        h1 {{ event.title }}
-        form(@submit.prevent="sendQuestion")
-          span Ask the speaker
-          a-textarea(
-            placeholder="Type your question"
-            :autoSize="{ minRows: 2, maxRows: 6 }"
-            v-model="question"
-          )
-          a-input(placeholder="Your name (optional)" v-model="name")
-          a-button(type="primary" @click="sendQuestion" :loading="loading" icon="message") Send
-          a-row(type="flex" justify="space-between")
-        a-layout-content.questions
-          a-row(type="flex" justify="space-between" align="middle")
-            a-col(span="8")
-              h2 Questions
-                a-avatar.question-count {{ questions.length }}
-            a-col(span="16" class="sort-container")
-              div(v-if="questions.length")
-                span Sort questions by &nbsp;
-                a-radio-group(size="small" defaultValue="popular" buttonStyle="solid" :value="sortBy")
-                  a-radio-button(value="popular" @click="updateSorting") Popular {{ popularSortOrderIndicator }}
-                  a-radio-button(value="recent" @click="updateSorting") Recent {{ recentSortOrderIndicator }}
-                  a-radio-button(value="random" @click="updateSorting") Random {{ randomSortOrderIndicator }}
-          a-row
-            p.no-questions(v-if="!questions.length") This event has no questions, be the first one and ask the first question!
-            a-card(v-for="question in questions" :key="question._id" :bodyStyle="{'padding-bottom': '8px'}")
-              a-comment
-                template(slot="actions")
-                  a-tooltip(:title="question.voted ? 'Dislike' : 'Like'" @click="vote({ questionId: question._id, vote: question.voted ? 'dislike' : 'like' })")
-                    span(key="comment-basic-like")
-                      a-icon(type="like" :theme="question.voted ? 'filled' : 'outlined'")
-                    span(style="padding-left: 4px") {{ question.votes }}
-                a(slot="author") {{ question.user }}
-                a-avatar(
-                  src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-                  alt="Han Solo"
-                  slot="avatar")
-                p(slot="content") {{ question.text }}
-                a-tooltip(slot="datetime" :title="moment(question.createdAt).format('YYYY-MM-DD HH:mm:ss')")
-                  span(:id="'question-' + question._id.slice(-4)") {{ moment(question.createdAt).fromNow() }}
-              div.question-id {{ '#' + question._id.slice(-4) }}
-    </a-tooltip>
+.home
+  .content
+    h1 {{ event.title }}
+    a-card
+      form(@submit.prevent="sendQuestion")
+        h2 Ask the speaker
+        a-textarea(
+          placeholder="Type your question"
+          :autoSize="{ minRows: 2, maxRows: 6 }"
+          v-model="question"
+        )
+        a-input(placeholder="Your name (optional)" v-model="name")
+        a-button(type="primary" @click="sendQuestion" :loading="loading" icon="message") Send
+    a-card
+      .questions
+        div.questions-header
+          h2 Questions
+            a-avatar.question-count {{ questions.length }}
+          .sort-container
+            div(v-if="questions.length")
+              span Sort questions by &nbsp;
+              a-radio-group(size="small" defaultValue="popular" buttonStyle="solid" :value="sortBy")
+                a-radio-button(value="popular" @click="updateSorting") Popular {{ popularSortOrderIndicator }}
+                a-radio-button(value="recent" @click="updateSorting") Recent {{ recentSortOrderIndicator }}
+                a-radio-button(value="random" @click="updateSorting") Random {{ randomSortOrderIndicator }}
+        div.questions-container
+          p.no-questions(v-if="!questions.length") This event has no questions, be the first one and ask the first question!
+          a-card(v-for="question in questions" :key="question._id" :bordered="false")
+            a-comment
+              template(slot="actions")
+                a-tooltip(:title="question.voted ? 'Dislike' : 'Like'" @click="vote({ questionId: question._id, vote: question.voted ? 'dislike' : 'like' })")
+                  span(key="comment-basic-like")
+                    a-icon(type="like" :theme="question.voted ? 'filled' : 'outlined'")
+                  span(style="padding-left: 4px") {{ question.votes }}
+              a(slot="author") {{ question.user }}
+              a-avatar(
+                src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                alt="Han Solo"
+                slot="avatar")
+              p(slot="content") {{ question.text }}
+              a-tooltip(slot="datetime" :title="moment(question.createdAt).format('YYYY-MM-DD HH:mm:ss')")
+                span(:id="'question-' + question._id.slice(-4)") {{ moment(question.createdAt).fromNow() }}
+            div.question-id {{ '#' + question._id.slice(-4) }}
+            hr
 </template>
+
+<style lang="scss">
+  .questions {
+    .ant-card {
+      .ant-card-body {
+        padding: 0px 24px;
+
+        hr {
+          height: 1px;
+          border: 0;
+          border-bottom: 1px solid #e8e8e8;
+        }
+      }
+    }
+  }
+</style>
 
 <style lang="scss" scoped>
   .home {
-    padding: 4em;
     min-height: 100vh;
+    background-color: #f6f6f6;
+    padding: 4em;
+    @media (max-width: 576px) {
+      padding: 1em;
+    }
+  }
+
+  .content {
+    max-width: 760px;
+    margin: 0 auto;
+    width: 100%;
+  }
+
+  .questions {
+    .questions-header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      @media (max-width: 680px) {
+        display: block;
+      }
+    }
+
+    .ant-card {
+      margin: 0;
+      padding-bottom: 16px;
+    }
+
+    .questions-container {
+      margin-top: 24px;
+    }
   }
 
   .no-questions {
@@ -153,16 +198,23 @@ a-layout.home
   }
 
   .sort-container {
-    display: flex;
-    justify-content: flex-end;
-    align-items: flex-end;
+    margin-top: 1em;
+  }
+
+  @media (min-width: 681px) {
+    .sort-container {
+      margin-top: 0;
+      display: flex;
+      justify-content: flex-end;
+      align-items: flex-end;
+    }
   }
 
   .question-id {
     position: absolute;
     right: 0em;
     top: 0em;
-    padding: 24px;
+    padding: 18px;
     font-size: 12px;
     line-height: 18px;
     color: #ccc;
