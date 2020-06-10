e170db0ab2486c35abdf46cfe156771c471a1645
Armagan Amcalar <armagan@amcalar.com>
2020-06-10 22:08:12 +0200

    Store computerId in User model

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const User = new mongoose.Schema({
   name: String,
   sessionId: String,
+  computerId: String,
   events: [{
     type: 'ObjectId',
     ref: 'Event'
