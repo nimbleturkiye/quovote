49765014ec79b115373e48dac712f79b887778e7
Armagan Amcalar <armagan@amcalar.com>
2020-07-08 22:35:33 +0200

    Fix voter decoration for voted users

--- backend/src/models/event.js
+++ backend/src/models/event.js
@@ -39,7 +39,7 @@ Event.pre('save', async function (next) {
 
 Event.static('decorateForUser', function (event, userIds) {
   const updatedQuestions = event.questions.toObject().map(q => {
-    return { ...q, voted: q.voters.some((v) => userIds.includes(v)), ownQuestion: userIds.some((i) => i.equals(q.user)), user: undefined, voters: undefined }
+    return { ...q, voted: q.voters.some((v) => userIds.some(id => id.equals(v))), ownQuestion: userIds.some((i) => i.equals(q.user)), user: undefined, voters: undefined }
   })
 
   return { ...event.toObject(), questions: updatedQuestions }
