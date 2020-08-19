5d803d6ca79eb34713b769e64f55a1f4da8d1750
Armagan Amcalar <armagan@amcalar.com>
2020-08-19 19:17:09 +0200

    Remove singularity index (#44)

--- backend/src/models/singularity.js
+++ backend/src/models/singularity.js
@@ -6,6 +6,4 @@ const Singularity = new mongoose.Schema({
   computerId: [String],
 })
 
-Singularity.index({ userId: 1, sessionId: 1, computerId: 1 }, { unique: true })
-
 module.exports = mongoose.model('Singularity', Singularity)
