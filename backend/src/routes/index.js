d3a3635b1079a4e12ce3ad6fbe69b070b92faccd
Dogukan Incesu <dogukanincesu@gmail.com>
2020-08-21 23:28:22 +0300

    Add slugify package

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,6 +6,7 @@ const socketServer = require('../socket-connection')
 const User = require('../models/user')
 const ensureSingularity = require('../lib/ensureSingularity')
 const ObjectId = require('mongoose').Types.ObjectId
+const slugify = require('slugify')
 
 async function ensureUser(req, res, next) {
   if (req.body.computerId) req.session.computerId = req.body.computerId
