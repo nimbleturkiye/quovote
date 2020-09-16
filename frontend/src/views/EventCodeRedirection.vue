889b21aadbff3709b29e73b576a51967d47b3439
Armagan Amcalar <armagan@amcalar.com>
2020-09-16 20:46:33 +0200

    Remove unnecessary redirection text and show an error on event code redirectoin

--- frontend/src/views/EventCodeRedirection.vue
+++ frontend/src/views/EventCodeRedirection.vue
@@ -4,22 +4,33 @@ import { notification } from 'ant-design-vue'
 
 export default {
   name: 'event-code-redirection',
+  data() {
+    return {
+      error: false
+    }
+  },
   async created () {
     const eventId = await this.fetchEventIdByCode(this.$route.params.eventCode)
 
     if (eventId) {
       this.$router.push({ name: 'eventDetail', params: { eventId } })
     } else {
+      this.error = true
       notification.error({ message: 'Event cannot be found' })
     }
   },
   methods: {
     ...mapActions(['fetchEventIdByCode'])
+  },
+  computed: {
+    code() {
+      return this.$route.params.eventCode
+    }
   }
 }
 </script>
 
 <template lang="pug">
-.home
-  p Redirecting to original event...
+div
+  p(v-if="error") Event with code {{ code }} could not be found.
 </template>
