7b74b4ccf9602ed7ec436912fc1a2a69eb61f498
Armagan Amcalar <armagan@amcalar.com>
2020-11-11 20:46:57 +0100

    Assign `nobiri-${uuid}` as computer id for users without biri support

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -9,6 +9,9 @@ const ObjectId = require('mongoose').Types.ObjectId
 
 async function ensureUser(req, res, next) {
   if (req.body.computerId) req.session.computerId = req.body.computerId
+  else {
+    req.session.computerId = req.session.computerId || `nobiri-${uuid()}`
+  }
 
   if (!req.session.userId) {
     if (req.user) req.session.userId = req.user._id
