a46f3df4ab692bcbf1bd2dc3bc897e7f5a064d66
Armagan Amcalar <armagan@amcalar.com>
2021-02-09 19:06:48 +0100

    Add useFindAndModify option to mongodb connection

--- backend/src/bootstrap.js
+++ backend/src/bootstrap.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 
 const connectionString = process.env.MONGO_CONNECTION_STRING || 'mongodb://mongodb:27017/quovote'
 
-mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 
 module.exports = { mongoose }
