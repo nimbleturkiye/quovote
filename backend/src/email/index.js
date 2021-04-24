daae6ba0db13f1d31c3b0b57d8e3344b70c7aa9f
Furkan Ilgenci <furkanilgenci@gmail.com>
2021-04-24 18:13:40 +0300

    Implmenet email service and verification email

---
+++ backend/src/email/index.js
@@ -0,0 +1,38 @@
+const nodemailer = require('nodemailer')
+const pug = require('pug')
+
+const transport = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+})
+
+async function send(payload) {
+  try {
+    await transport.sendMail(payload)
+  } catch (err) {
+    console.log(err)
+    throw new Error('There is a problem sending email. Please try again later.')
+  }
+}
+
+async function sendEmailVerification(email, name, token) {
+  if (!name || !token) throw new Error('Name and token must be provided.')
+
+  const verificationUrl = `${process.env.BACKEND_BASE_PATH}/account/email-verification?token=${token}`
+
+  await send({
+    from: '"QuoVote Team" <noreply@quo.vote>',
+    to: email,
+    subject: 'Verify your QuoVote account',
+    html: pug.renderFile(`${__dirname}/templates/verify-email.pug`, {
+      name,
+      verificationUrl,
+    }),
+  })
+}
+
+module.exports = { sendEmailVerification }
