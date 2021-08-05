bdde7fe5b25e8c08f4c9c52807f3d847f5a5b1dd
RahimCml <rhmcml@mail.ru>
2021-08-05 17:35:56 +0400

    Add linter to backend(#103)

---
+++ backend/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true
+  },
+  extends: ['standard', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  plugins: ['prettier'],
+  rules: {}
+}
